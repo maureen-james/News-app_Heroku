47042ba9f6602f8abc2383c9ff58e574d7eff340
maureen-james <manjax97@gmail.com>
2022-04-29 16:06:52 +0300

    pass title in our template

--- app/views.py
+++ app/views.py
@@ -19,4 +19,13 @@ def news(news_id):
     '''
     View news page function that returns the news details page and its data
     '''
-    return render_template('news.html',id = news_id)    
+    return render_template('news.html',id = news_id)  
+
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to The best news Review Website Online'
+    return render_template('index.html', title = title)
