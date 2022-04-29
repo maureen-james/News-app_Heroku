986ca110070983c976bea778ec65162bed5578f5
maureen-james <manjax97@gmail.com>
2022-04-29 16:36:23 +0300

    add base url

--- app/config.py
+++ app/config.py
@@ -25,3 +25,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+class Config:
+    '''
+    General configuration parent class
+    '''
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
