10bb59d49949ac6e84632b29574ca36f3b4c2b08
maureen-james <manjax97@gmail.com>
2022-04-29 14:40:37 +0300

    add a variable block

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 
+
 # Views
 @app.route('/')
 def index():
@@ -8,4 +9,6 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    message = 'Hello World'
+    return render_template('index.html',message = message)
