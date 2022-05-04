6aba9adfe64683ca62984157ea982c5cdd171d79
maureen-james <manjax97@gmail.com>
2022-05-04 11:08:44 +0300

    modify start.sh file

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY = '044e110d04e84886a4057da519c842af'
+export NEWS_API_KEY = '0a94628023534ae0a969eb3432d2e8a9'
 export SECRET_KEY   = 'M1A2U3r4E5e6N7N8cY9bA0M1B2aU3R4A'
 
 python3.8 manage.py server
